7d18e6ead08f852d3f06b094152f5626b91d3fae
李元明 <liyuanming@coding.net>
2020-06-19 11:19:59 +0800

    update shell

--- openAPIOauthTest.sh
+++ openAPIOauthTest.sh
@@ -10,16 +10,16 @@ for example:
 \"client_id=xxx\"
 \"client_secret=xxx\"
 \"redirect_uri=xxx\"
-\"team_gk=xxx\"
+\"team_url=xxx\"
 "
 
-client_id="48d838ba2180a6f52ba6375fff2491b8"
-client_secret="56fc7673c8ad750bba52eb847cb022a478826174"
+client_id="773be67cb424e4341386eb74a0818315"
+client_secret="41e05f3bc6deb3a9902715d28a3d663a3c8a151b"
 #回调地址
 #可以在https://requestbin.com/ 创建免费服务器地址作为回调地址
-redirect_uri="https://endinvg2m90oa.x.pipedream.net"
-#团队域名: test.coding.com 里 test 为团队域名
-team_gk="codingtestkindli"
+redirect_uri="https://enx41noa8mm1.x.pipedream.net"
+#团队域名: http://henry.coding.com 末尾不能加 /
+team_url="http://codingcorp.staging-corp.coding.io"
 
 read -p "are you ready? y/n
 " isReady
@@ -34,7 +34,7 @@ fi
 
 echo "here is request for chrome"
 echo ""
-req1="https://${team_gk}.coding.net/oauth_authorize.html?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code"
+req1="${team_url}/oauth_authorize.html?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code"
 echo $req1
 echo ""
 
@@ -47,12 +47,13 @@ then
     echo "ERROR: code is empty"
     exit 1;
 fi
-req2="http://${team_gk}.coding.net/api/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&code=${code}"
+req2="${team_url}/api/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&code=${code}"
 
 echo "
 your access_token request:
 
 ${req2}"
+echo ""
 
 #access_token=`curl "${req2}"`
 #echo "access_token=${access_token}"
